32f2a7b4e84e2586cb201dc6e8f95218c3b77a27
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2016-12-25 20:41:45 +0300

    crackling: xosp: Bringup
    Change-Id: I0468536bc29999d6a259008b5552328dab110685

--- xosp.mk
+++ xosp.mk
@@ -14,13 +14,14 @@
 
 $(call inherit-product, device/wileyfox/crackling/full_crackling.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit common XOSP stuff.
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := cm_crackling
+PRODUCT_NAME := xosp_crackling
 BOARD_VENDOR := wileyfox
 PRODUCT_DEVICE := crackling
 
