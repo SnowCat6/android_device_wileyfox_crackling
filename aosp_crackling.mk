92cc71296ea4f5ac3f2967e79ea47e588c79ccdc
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2016-12-28 07:28:56 +0300

    crackling: Fix build
    Change-Id: I96047d7288a99b9b4d6c6e281bbe9488581b3593

--- aosp_crackling.mk
+++ aosp_crackling.mk
@@ -14,8 +14,8 @@
 
 $(call inherit-product, device/wileyfox/crackling/full_crackling.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
