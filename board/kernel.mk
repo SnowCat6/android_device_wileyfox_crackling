89289871bf9612daea3e331dd242ac5e23a59633
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2016-12-28 07:35:06 +0300

    crackling: We need appended dtb image
    Change-Id: Iac47257b1f5c775dc91ba4b18856542a02ce8559

--- board/kernel.mk
+++ board/kernel.mk
@@ -4,3 +4,4 @@ BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_CONFIG := cyanogenmod_crackling_defconfig
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
