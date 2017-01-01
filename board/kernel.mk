3b0e25d6cb38b54531380d14268341750c4ab507
Ilya Lebedev <ilya.lebedev.2003@mail.ru>
2017-01-01 13:36:44 +0300

    crackling: zos: ZOS use another toolchain
    Change-Id: I5b42ba6f6dba20ce15bdfbf2945b4ba0f40909fd

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,6 +2,6 @@
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_CONFIG := cyanogenmod_crackling_defconfig
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
